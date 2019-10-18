e93cbd1fcb6ec935375a77895763709b13cac04c
smulard <sophie.jmulard@gmail.com>
2019-10-18 10:52:01 -0400

    rm old directory structure

--- microblog/app/microblog.py
+++
@@ -1,8 +0,0 @@
-'''
-Script at the top-level that defines the Flask application instance.
-All it does is imports the application instance.
-The Falsk application instance is called 'app' and is a memeber of the app package.
-The 'from app import app' statement imports the 'app' variable that is a member of the 'app' package.
-'''
-
-from app import app
